0154c10d42a44095c4e11eabb7299451324b1bc6
Gonzalo Flamenci <gonzaloflamenci4@gmail.com>
2022-06-23 20:03:59 -0300

    remove db connect function

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ app.use(router);
 app.use(express.static(join(__dirname, "public/css")));
 
 app.listen(PORT, async () => {
-  await db();
   console.log(`
 ====================================
         Listen on port ${PORT}
